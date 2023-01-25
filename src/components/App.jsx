2f3e714e06e268552c3dbeb7a3da14dd4b282462
Veronica <vveronikanika@gmail.com>
2023-01-25 16:22:19 +0200

    refactor: delete unnesessary imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,12 @@
 import { lazy, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getContactsThunk } from 'redux/contacts/operations/contactsThunk';
-import { selectContacts } from 'redux/contacts/selectors';
-import styles from './App.module.css';
-// import { ContactForm } from './ContactForm/ContactForm';
-// import { ContactList } from './ContactList/ContactList';
-// import { Filter } from './Filter/Filter';
 import { Route, Routes } from 'react-router-dom';
-
 import { Layout } from './Layout';
 import { PrivateRoute } from './PrivateRoute';
 import { RestrictedRoute } from './RestrictedRoute';
 import { useAuth } from 'hooks/useAuth';
+import styles from './App.module.css';
 
 const HomePage = lazy(() => import('../pages/HomePage'));
 const RegisterPage = lazy(() => import('../pages/Register'));
@@ -27,22 +22,11 @@ export const App = () => {
     dispatch(getContactsThunk());
   }, [dispatch]);
 
-  // const users = useSelector(selectContacts);
-
   return isRefreshing ? (
     <b>Refreshing user...</b>
   ) : (
     <section className={styles.phonebook}>
       <div className={styles.container}>
-        {/* <h1>Phonebook</h1>
-        <ContactForm />
-      </div>
-      <div className={styles.container}>
-        <h2>Contacts</h2>
-          <h3>Your phonebook has {users.length} contacts</h3>
-          <Filter />
-          <ContactList /> */}
-
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
